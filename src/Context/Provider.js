46705b48c46717569f4fff6a790e3a48044124cf
Emerson Florencio <emersonflorencio@hotmail.com>
2022-09-24 12:58:32 -0300

    Resolvendo requisito 1

---
+++ src/Context/Provider.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import starWarsPlanetsContext from './ContextStarWarsPlanets';
+
+const ENDPOINT = 'https://swapi.dev/api/planets';
+
+function Provider({ children }) {
+  const [planetsApi, setPlanetsApi] = useState([]);
+
+  useEffect(() => {
+    const getApiPlanets = async () => {
+      const response = await fetch(ENDPOINT);
+      const data = await response.json();
+      const filterdata = data.results.map((info) => {
+        delete info.residents;
+        return info;
+      });
+      setPlanetsApi(filterdata);
+      console.log(filterdata);
+    };
+    getApiPlanets();
+  }, []);
+
+  const contextValue = {
+    planetsApi,
+  };
+
+  return (
+    <starWarsPlanetsContext.Provider value={ contextValue }>
+      {children}
+    </starWarsPlanetsContext.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
